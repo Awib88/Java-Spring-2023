1471790c35caaadb6bc1f5d9392136580faa3235
awib <aminwahib98@gmail.com>
2023-11-08 17:38:54 +0100

    first commit

---
+++ java fundamentals/First java program/FirstJava.java
@@ -0,0 +1,8 @@
+
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("My name is Amin");
+        System.out.println("I am 24 years old");
+        System.out.println("My hometown is Djibouti,Dj");
+    }
+}
